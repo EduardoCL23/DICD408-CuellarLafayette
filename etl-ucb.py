e04a6d42c289c2da0c4f1ce0ec5e2eb687b64da8
EduardoCL23 <eduardo.cuellar@ucb.edu.bo>
2024-05-25 01:41:34 +0000

    ETLviernes

--- etl-ucb.py
+++ etl-ucb.py
@@ -30,6 +30,13 @@ print(wine_quality_data.isnull().sum())
 wine_data['alcohol'] = (wine_data['alcohol'] - wine_data['alcohol'].min()) / (wine_data['alcohol'].max() 
 - wine_data['alcohol'].min())
 
+wine_data["alcohol"] = wine_data["alcohol"]*1
+
+# Creating an average quality column in wine_quality_data
+wine_quality_data['average_quality'] = wine_quality_data[['fixed acidity', 'volatile acidity', 'citric acid',
+                                                          'residual sugar', 'chlorides', 'free sulfur dioxide',
+                                                          'total sulfur dioxide', 'density', 'pH', 'sulphates',
+                                                          'alcohol']].mean(axis = 1)
 
 
 # Loading
